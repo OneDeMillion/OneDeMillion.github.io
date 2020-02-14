96db9a24ec401e0bb26adea5194ea302ed8e0c97
Use your GitHub username <Enter your email address>
2020-02-14 14:30:39 -0600

    s

--- lesson6/js/toggleMenu.js
+++ lesson6/js/toggleMenu.js
@@ -1,5 +1,5 @@
 const menuButton = document.querySelector(".menuButton");
-menuButton.addEventListener("click", toggleMenu, false);
+menuButton.addEventListener(click, toggleMenu, false);
 
 function toggleMenu() {
     document.querySelector("*.primaryNav").classList.toggle("respond");
