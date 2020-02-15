a112bf8ef4262190ff7453be164e3465af8a7e8c
Use your GitHub username <Enter your email address>
2020-02-15 13:33:05 -0600

    f

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,12 +2,12 @@ let t = parseFloat(document.getElementById("highTemp").innerText);
 //console.log(t);
 let s = parseFloat(document.getElementById("windSpd").innerText);
 //console.log(s)
-let w = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.04275 * t * Math.pow(s,0.16));
+let w = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * Math.pow(s,0.16));
 //console.log(w)
 
 if(t <= 50 && s > 3) {
 
-document.getElementById("windChill").innerText=w.toFixed(0) ;
+document.getElementById("windChill").innerText=w.toFixed(0) + "&#176;F;"
 
 }
 
