75bd091ed379ef3dd1ba399c6c3b78dd62ed66ab
Use your GitHub username <Enter your email address>
2020-03-03 09:58:34 -0600

    s

--- lesson8/js/scripts.js
+++ lesson8/js/scripts.js
@@ -1,3 +1,7 @@
-function adjustRating(severity) {
-    document.getElementById("ratingValue").innerHTML = severity;
+var severity = document.getElementById('severity');
+var output = document.getElementById("ratingValue");
+output.innerText = slider.value; 
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
 }
